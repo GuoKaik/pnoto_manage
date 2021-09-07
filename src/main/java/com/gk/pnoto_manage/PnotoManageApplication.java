7dd27ac50da159ac62b7fe5a60ed0d976d4d562a
834108798@qq.com <guokai1900>
2021-09-07 22:33:56 +0800

    aaa

---
+++ src/main/java/com/gk/pnoto_manage/PnotoManageApplication.java
@@ -0,0 +1,13 @@
+package com.gk.pnoto_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnotoManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PnotoManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gk/pnoto_manage/PnotoManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk.pnoto_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnotoManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
